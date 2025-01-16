614b4e004bf9d452b4b678836c4f2e196284f79e
Shashank <shashankbmusale07@gmail.com>
2025-01-16 15:57:00 +0530

    upd

--- backend/server.js
+++ backend/server.js
@@ -3,10 +3,18 @@ const dotenv = require("dotenv").config();
 const { errorHandler } = require("./middleware/errorMiddleware");
 const connectDB = require("./config/db");
 const port = process.env.PORT || 5000;
+const cors = require("cors");
 
 connectDB();
 
 const app = express();
+
+app.use(
+  cors({
+    origin: "https://mern-jwt-app.vercel.app/login",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
